ed8481ca9cfbf4151aa6f54f1767210fc22bde6e
MRyzhylo <playa77777@gmail.com>
2019-09-26 12:05:25 +0300

    Updates

---
+++ precache-manifest.a7ded37c611d27aea576b83ea80246e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220a230b6daa7e9fde896fe5fbe63a16",
+    "url": "/wss/index.html"
+  },
+  {
+    "revision": "7b3668579943a65b3a5d",
+    "url": "/wss/static/css/main.c1702ead.chunk.css"
+  },
+  {
+    "revision": "0256a523f34af564284e",
+    "url": "/wss/static/js/2.ccbbd2b6.chunk.js"
+  },
+  {
+    "revision": "7b3668579943a65b3a5d",
+    "url": "/wss/static/js/main.03d932ea.chunk.js"
+  },
+  {
+    "revision": "d00edf36f2482efa1823",
+    "url": "/wss/static/js/runtime~main.11e75558.js"
+  },
+  {
+    "revision": "c0842c929806a9fdb6d83d29ba177ea9",
+    "url": "/wss/static/media/logo-2.c0842c92.jpg"
+  }
+]);
